e0d7b2fe80563e093a4cb54f6c3fa6404fa23404
stonewild33 <36134873+stonewild33@users.noreply.github.com>
2018-03-06 17:00:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var left = 0;
 function moveright()
 {   
     left = left + 10;
-    img.style.moveLeft= left + px;
+    img.style.moveLeft= left + 'px';
 }
 img.onclick = function() {
     var interval = setInterval(moveright,100);
