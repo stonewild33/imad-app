4a9f5d0614a4af022fcac4630eb7b2255622a98c
stonewild33 <36134873+stonewild33@users.noreply.github.com>
2018-03-06 16:51:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,5 @@ e.innerHTML='saket the great';
 var img = document.getElementById('madi');
 
 img.onclick = function() {
-    img.style.backgroundcolor ='blue';
+    img.style.marginleft ='200px';
 };
