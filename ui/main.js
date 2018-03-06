1dfdb3712c8de1fc844885e5be0a13af93d7126e
stonewild33 <36134873+stonewild33@users.noreply.github.com>
2018-03-06 16:49:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var e = document.getElementById('saket');
 e.innerHTML='saket the great';
 
-var img =document.getElementById('madi');
+var img = document.getElementById('madi');
 
 img.onclick = function() {
     img.style.backgroundcolor ='blue';
