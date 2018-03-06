992825c841ed347e1f5f464ac44ac9c7735b5514
stonewild33 <36134873+stonewild33@users.noreply.github.com>
2018-03-06 16:46:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var e = document.getElementById('saket');
 e.innerHTML='saket the great';
+
+var img =document.getElementById('madi');
+
+img.onclick = function() {
+    img.style.rightmargin='100px';
+}
