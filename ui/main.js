c0b5393289f61d4db4ec1a0624ef2e4012489d09
stonewild33 <36134873+stonewild33@users.noreply.github.com>
2018-03-13 15:34:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
 
-var app = express();
-app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/article-one', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
@@ -20,28 +8,3 @@ app.get('/counter', function (req, res) {
 });
 
 
-
-app.get('/article-two', function (req, res) {
-  res.send('Articcle two is requested');
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
