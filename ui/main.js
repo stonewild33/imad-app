020d6103a5d3b694cb556324bd475594e5080fa4
stonewild33 <36134873+stonewild33@users.noreply.github.com>
2018-03-15 19:41:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 
 
-var button=document.getElementById(counter);
+var button=document.getElementById('counter');
 
 var counter = 0;
 button.onClick =function() {
@@ -11,7 +11,7 @@ button.onClick =function() {
   // Render the varible in correct span;
   
   counter = counter + 1; 
-  var span = document.getElementById(count);
+  var span = document.getElementById('count');
     
     span.innerHTMl= counter.toString();
 };
